1da914c188a3472aaacddd4141d96a40afaab85a
Giddy4Tech <osimgideon09@outlook.com>
2023-04-03 12:29:23 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _strpbrk - Searches a string for any of a set of bytes.
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * print_diagsums - Prints the sum of the two diagonals of a square.
